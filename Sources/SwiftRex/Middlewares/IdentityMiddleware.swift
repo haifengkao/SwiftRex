7ea390827812783a8d607fb4c75b027f051bea86
Hai Feng Kao <khaifeng@gmail.com>
2025-03-25 22:29:40 +0800

    Sendable and MainActor madness

--- Sources/SwiftRex/Middlewares/IdentityMiddleware.swift
+++ Sources/SwiftRex/Middlewares/IdentityMiddleware.swift
@@ -19,6 +19,7 @@ public struct IdentityMiddleware<InputActionType, OutputActionType, StateType>: 
        - state: a closure to obtain the most recent state
      - Returns: possible Side-Effects wrapped in an IO struct
      */
+    @MainActor
     public func handle(action: InputActionType, from dispatcher: ActionSource, state: @escaping GetState<StateType>) -> IO<OutputActionType> {
         .pure()
     }
