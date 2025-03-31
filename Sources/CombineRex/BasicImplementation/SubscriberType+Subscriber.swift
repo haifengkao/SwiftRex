f1d881fc29b77355fa728072012062a84597fb00
Hai Feng Kao <khaifeng@gmail.com>
2025-04-01 03:29:11 +0800

    add combine func for testing

---
+++ Sources/CombineRex/BasicImplementation/SubscriberType+Subscriber.swift
@@ -0,0 +1,23 @@
+
+import SwiftRex
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+extension SubscriberType {
+
+
+    public static func combine<Input, Failure>(onValue: (@MainActor (Input) -> Void)? = nil, onCompleted: (@MainActor (Failure?) -> Void)? = nil)
+        -> SubscriberType<Input, Failure> {
+            
+            SubscriberType<Input, Failure>(onValue: onValue ?? { _ in },
+                           onCompleted: { error in
+                guard let error else {
+                    onCompleted?(nil)
+                    return
+                }
+                onCompleted?(error)
+               
+            })
+      
+    }
+}
+
