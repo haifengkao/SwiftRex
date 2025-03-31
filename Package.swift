c6e45fa145bbef2fd3c2e68826a09f0074960389
Hai Feng Kao <khaifeng@gmail.com>
2025-04-01 00:57:18 +0800

    update package.swift

--- Package.swift
+++ Package.swift
@@ -5,32 +5,22 @@ let package = Package(
     name: "SwiftRex",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
     products: [
-        .library(name: "CombineRex", targets: ["SwiftRex"])
-        // .library(name: "CombineRex", targets: ["SwiftRex", "CombineRex"]),
-        // .library(name: "ReactiveSwiftRex", targets: ["SwiftRex", "ReactiveSwiftRex"]),
-        // .library(name: "RxSwiftRex", targets: ["SwiftRex", "RxSwiftRex"]),
-
-        // .library(name: "CombineRexDynamic", type: .dynamic, targets: ["SwiftRex", "CombineRex"]),
-        // .library(name: "ReactiveSwiftRexDynamic", type: .dynamic, targets: ["SwiftRex", "ReactiveSwiftRex"]),
-        // .library(name: "RxSwiftRexDynamic", type: .dynamic, targets: ["SwiftRex", "RxSwiftRex"])
+        .library(name: "CombineRex", targets: ["SwiftRex", "CombineRex"]),
+        .library(name: "CombineRextensions", targets: ["CombineRextensions"])
     ],
     dependencies: [
-        // .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.2.0"),
-        // .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "7.0.0")
+        .package(url: "https://github.com/TeufelAudio/UIExtensions.git", from: "4.0.0")
     ],
     targets: [
         .target(
             name: "SwiftRex",
             exclude: ["CodeGeneration/Templates"]
         ),
-        // .target(name: "CombineRex", dependencies: ["SwiftRex"]),
-        // .target(name: "ReactiveSwiftRex", dependencies: ["SwiftRex", "ReactiveSwift"]),
-        // .target(name: "RxSwiftRex", dependencies: ["SwiftRex", "RxSwift"]),
+        .target(name: "CombineRex", dependencies: ["SwiftRex"]),
+        .target(name: "CombineRextensions", dependencies: ["CombineRex"]),
 
-        .testTarget(name: "SwiftRexTests", dependencies: ["SwiftRex"])
-        // .testTarget(name: "CombineRexTests", dependencies: ["CombineRex"]),
-        // .testTarget(name: "ReactiveSwiftRexTests", dependencies: ["ReactiveSwiftRex"]),
-        // .testTarget(name: "RxSwiftRexTests", dependencies: ["RxSwiftRex"])
+        .testTarget(name: "SwiftRexTests", dependencies: ["SwiftRex"]),
+        .testTarget(name: "CombineRexTests", dependencies: ["CombineRex"]),
     ],
     swiftLanguageModes: [.v6]
 )
