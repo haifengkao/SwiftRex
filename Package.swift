1c9dcc4d27dc556cbe3aaa9c15f520a7fb2cd5bc
Hai Feng Kao <khaifeng@gmail.com>
2025-04-02 18:58:29 +0800

    Remove BridgeMiddleware and CombineRextension
    Tuist is unhappy about multiple modules in the same package

--- Package.swift
+++ Package.swift
@@ -5,9 +5,7 @@ let package = Package(
     name: "SwiftRex",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
     products: [
-        .library(name: "CombineRex", targets: ["SwiftRex", "CombineRex"]),
-        .library(name: "CombineRextensions", targets: ["CombineRextensions"]),
-        .library(name: "BridgeMiddleware", targets: ["BridgeMiddleware"])
+        .library(name: "CombineRex", targets: ["SwiftRex", "CombineRex"])
     ],
     dependencies: [
     ],
@@ -17,8 +15,6 @@ let package = Package(
             exclude: ["CodeGeneration/Templates"]
         ),
         .target(name: "CombineRex", dependencies: ["SwiftRex"]),
-        .target(name: "BridgeMiddleware", dependencies: ["SwiftRex"]),
-        .target(name: "CombineRextensions", dependencies: ["CombineRex"]),
 
         .testTarget(name: "SwiftRexTests", dependencies: ["SwiftRex"]),
         .testTarget(name: "CombineRexTests", dependencies: ["CombineRex"])
